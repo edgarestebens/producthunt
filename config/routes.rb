41f6ba1bbdd6c88456d9ea55795f2e8cefde6f80
edgar <colachoedgar@hotmail.com>
2018-11-11 06:41:05 -0600

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-	#get '/products', to: 'products#index'
-	#get '/products/new', to: 'products#new', as: 'new_product'
-	#post '/products', to: 'products#create'
-	#get '/products/:id', to: 'products#show', as:'product'
-	#get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-	#patch '/products/:id', to: 'products#update'
-	#delete '/products/:id', to: 'products#destroy'
-	 
+  
+    root 'products#index' #arranca como pagina inicial el index
+
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+
+    resources :users, only: [:new, :create]
+    resources :products
+  
+	resources :users, only: [:new, :create] 
 	resources :products #genera por debajo las rutas que estan arriba
   	
-  	root 'products#index' #arranca como pagina inicial el index
-
-  	#resources :products, only: [:index] solo mostraria la ruta de index
-  	#resources :products, only: [:index, :create] mostraria index y create
-
 end
